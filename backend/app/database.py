878c0918c895e17219ea284ddc8eb3fcc7627e3c
rahi-32 <danishahamad.amil78@gmail.com>
2025-11-22 21:58:05 +0530

    update library

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./governance.db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://govadmin:Genadmin-1@34.47.128.149:5432/governance_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# No connect_args needed for Postgres
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
